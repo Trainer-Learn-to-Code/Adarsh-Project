4a3fa0e070b85ae8a1b9790581cc4c0a91835901
Trainer-Learn-to-Code <80373993+Trainer-Learn-to-Code@users.noreply.github.com>
2022-08-16 11:31:05 +0530

    Add files via upload

---
+++ Online food ordering system/src/main/java/db/Database.java
@@ -0,0 +1,9 @@
+package db;
+
+public class Database {
+public static final String driverClass="com.mysql.jdbc.Driver";
+public static final String connectionUrl="jdbc:mysql://localhost:3306/mysql";
+public static final String username="root";
+public static final String password="";
+
+}
